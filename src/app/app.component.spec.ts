05531afe9fa5eb93601c0115915ad90092f5140d
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2024-09-25 10:55:15 +0200

    add an article component to my app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ArticleComponentComponent } from './article-component/article-component.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, ArticleComponentComponent],
     }).compileComponents();
   });
 
@@ -24,6 +25,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, wild-blog');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, wild-blog'
+    );
   });
 });
