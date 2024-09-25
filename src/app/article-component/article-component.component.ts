c5f8bf5add4de6019eff54d3404e347b746f6917
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2024-09-25 14:58:40 +0200

    add binding for article

--- src/app/article-component/article-component.component.ts
+++ src/app/article-component/article-component.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+export interface Article {
+  title: string;
+  author: string;
+  content: string;
+  image: string;
+  isPublished: boolean;
+  comment: string;
+}
 
 @Component({
   selector: 'app-article-component',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './article-component.component.html',
-  styleUrl: './article-component.component.scss'
+  styleUrl: './article-component.component.scss',
 })
 export class ArticleComponentComponent {
+  article: Article = {
+    title: 'My wonderful Article !',
+    author: 'Manu B.',
+    content: "This is the best article, you've never seen before !",
+    image: 'https://via.placeholder.com/350x150',
+    isPublished: true,
+    comment: '',
+  };
 
+  togglePublication(): void {
+    this.article.isPublished = !this.article.isPublished;
+  }
 }
