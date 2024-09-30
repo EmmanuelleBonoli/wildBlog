8e6c734b603febfc4ece83d458f09a23414ca386
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2024-09-30 16:22:03 +0200

    add template driven forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ArticleComponentComponent } from './article-component/article-component.component';
 import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'article/:id', component: ArticleComponentComponent },
+  { path: 'contact', component: ContactFormComponent },
   { path: '**', component: NotFoundComponentComponent },
 ];
