45978dbda00e0d99f542d25c3fa83cde190a8d8e
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2024-10-01 14:59:01 +0200

    add @input for header

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -4,17 +4,20 @@ import {
   ArticleService,
   Article,
 } from '../service/article-service/article-service.component';
-
+import { ArticleComponentComponent } from '../article-component/article-component.component';
+import { HeaderComponent } from '../header/header.component';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ArticleComponentComponent, HeaderComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
   articles: Article[] = [];
   articleService: ArticleService = inject(ArticleService);
+
+  statusUser: boolean = true;
 
   ngOnInit() {
     this.articles = this.articleService.getArticles();
